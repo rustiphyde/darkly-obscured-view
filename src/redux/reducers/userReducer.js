e07be3020ff594dc5b2e5145a4ea7475fa6b3c4e
Rusty Hoppins <rustiphyde@gmail.com>
2021-04-30 16:39:59 -0500

    Add userReducer file

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,40 @@
+import {
+    SET_USER,
+    SET_AUTHENTICATED,
+    SET_UNAUTHENTICATED,
+    LOADING_USER,
+  } from "../types";
+  
+  const initialState = {
+    authenticated: false,
+    credentials: {},
+    loading: false,
+  };
+  
+  export default function(state = initialState, action) {
+    // perform appropriate actions according to type
+    switch (action.type) {
+      //catch the various type cases
+      case SET_AUTHENTICATED:
+        return {
+          // spreads the state as it already exists and then changes certain elements as specified
+          ...state,
+          authenticated: true
+        };
+      case SET_UNAUTHENTICATED:
+        return initialState;
+      case SET_USER:
+        return {
+          authenticated: true,
+          loading: false,
+          ...action.payload
+        };
+      case LOADING_USER:
+        return {
+          ...state,
+          loading: true
+        };
+      default:
+        return state;
+    }
+  }
