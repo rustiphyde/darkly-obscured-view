1071134d325570dc0a6b47d0f9dcbd1d830d7470
Rusty Hoppins <rustiphyde@gmail.com>
2021-04-30 16:33:15 -0500

    Add redux folder with types.js file

---
+++ src/redux/store.js
@@ -0,0 +1,37 @@
+// store file "stores" the application state
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { config } from '../util/config';
+import { firestoreReducer, getFirestore, reduxFirestore } from 'redux-firestore';
+import { firebaseReducer, getFirebase } from 'react-redux-firebase';
+import userReducer from "./reducers/userReducer";
+import investigatorReducer from "./reducers/investigatorReducer";
+import dataReducer from "./reducers/dataReducer";
+import uiReducer from "./reducers/uiReducer";
+
+const initialState = {};
+
+const middleware = [thunk.withExtraArgument({ getFirestore, getFirebase })];
+
+// Actual state 
+const reducers = combineReducers({
+  // Everything that comes from userReducer.js will be stored inside the user object
+  user: userReducer,
+  // Everything that comes from investigatorReducer.js will be stored inside the investigator object
+  investigator: investigatorReducer,
+  // Everything that comes from dataReducer.js will be stored inside the data object
+  data: dataReducer,
+  // Everything that comes from uiReducer.js will be stored inside the UI object
+  UI: uiReducer,
+  // Everthing that comes from firebaseReducer will be stored inside the firebase object
+  firebase: firebaseReducer,
+  // Everthing that comes from firestoreReducer will be stored inside the firestore object
+  firestore: firestoreReducer
+});
+
+const composeEnhancers = compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware), reduxFirestore(config));
+const store = createStore(reducers, initialState, enhancer);
+
+export default store;
---
+++ src/redux/types.js
@@ -0,0 +1,22 @@
+// Investigator Reducer Types
+export const SET_AUTHENTICATED_INV = 'SET_AUTHENTICATED_INV';
+export const SET_UNAUTHENTICATED_INV = 'SET_UNAUTHENTICATED_INV';
+export const SET_INVEST = 'SET_INVEST';
+export const LOADING_INV = 'LOADING_INV';
+// User Reducer Types
+export const SET_AUTHENTICATED_USER ='SET_AUTHENTICATED_USER';
+export const SET_UNAUTHENTICATED_USER = 'SET_UNAUTHENTICATED_USER';
+export const SET_USER = 'SET_USER';
+export const LOADING_USER = 'LOADING_USER';
+// UI Reducer Types
+export const SET_ERRORS = 'SET_ERRORS';
+export const LOADING_UI = 'LOADING_UI';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const STOP_LOADING_UI = 'STOP_LOADING_UI';
+export const SET_SUCCESS = 'SET_SUCCESS';
+export const CLEAR_SUCCESS = 'CLEAR_SUCCESS';
+// Data Reducer Types
+export const SET_INVESTIGATIONS = 'SET_INVESTIGATIONS';
+export const SET_INVESTIGATION = 'SET_INVESTIGATION';
+export const LOADING_DATA = 'LOADING_DATA';
+
