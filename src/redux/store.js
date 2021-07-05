12eeeda4fe86afd9cf8abbe019b7e9561ea37585
Rusty Hoppins <rustiphyde@gmail.com>
2021-07-05 16:54:19 -0500

    Add redux-devtools-extension to store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 // store file "stores" the application state
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { config } from '../util/config';
 import { firestoreReducer, getFirestore, reduxFirestore } from 'redux-firestore';
 import { firebaseReducer, getFirebase } from 'react-redux-firebase';
-import userReducer from "./reducers/userReducer";
 import investigatorReducer from "./reducers/investigatorReducer";
 import dataReducer from "./reducers/dataReducer";
 import uiReducer from "./reducers/uiReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 
@@ -15,10 +15,8 @@ const middleware = [thunk.withExtraArgument({ getFirestore, getFirebase })];
 
 // Actual state 
 const reducers = combineReducers({
-  // Everything that comes from userReducer.js will be stored inside the user object
-  user: userReducer,
   // Everything that comes from investigatorReducer.js will be stored inside the investigator object
-  investigator: investigatorReducer,
+  invest: investigatorReducer,
   // Everything that comes from dataReducer.js will be stored inside the data object
   data: dataReducer,
   // Everything that comes from uiReducer.js will be stored inside the UI object
@@ -29,9 +27,9 @@ const reducers = combineReducers({
   firestore: firestoreReducer
 });
 
-const composeEnhancers = compose;
 
-const enhancer = composeEnhancers(applyMiddleware(...middleware), reduxFirestore(config));
+
+const enhancer = composeWithDevTools(applyMiddleware(...middleware), reduxFirestore(config));
 const store = createStore(reducers, initialState, enhancer);
 
 export default store;
